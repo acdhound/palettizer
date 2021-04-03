16d463172fca1b5beab64922b096baa346e8fd39
acdhound <adivanov95@mail.ru>
2021-04-03 16:00:52 +0300

    introduce command line arguments, cleanup code

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from PIL import Image
 from quantize import quantize
 from palette import parse_palette
+import sys
 
-# if __name__ == '__main__':
-#     print_hi('PyCharm')
+if __name__ != '__main__':
+    print("The script isn't executed as main, terminating")
+    exit(1)
 
-print("Parsing the palette...")
-palette = parse_palette('arton-palette.json')
+if len(sys.argv) < 4:
+    raise Exception('Expected 3 arguments: input image path, palette file path and output image path')
+input_img = sys.argv[1]
+palette = sys.argv[2]
+output_img = sys.argv[3]
+
+print('Parsing the palette from ' + palette + '...')
+palette = parse_palette(palette)
 print("Successfully parsed")
 
-print('Opening image...')
-im = Image.open("testimage2.png")
+print('Opening image from ' + input_img + '...')
+im = Image.open(input_img)
 print('Image opened')
 print(im.format, im.size, im.mode)
 
 print('Quantizing the image...')
-quantize(im, palette
-         # [(0, 0, 0),
-         #      (255, 255, 255),
-         #      (255, 255, 0),
-         #      (255, 0, 255),
-         #      (0, 255, 255),
-         #      (255, 0, 0),
-         #      (0, 255, 0),
-         #      (0, 0, 255)]
-)
+quantize(im, palette)
 print('Quantization finished')
 
-print('Saving the quantized image...')
-im.save('outputimage.png')
+print('Saving the quantized image to ' + output_img + '...')
+im.save(output_img)
 print('Successfully saved')
 
 exit(0)
--- quantize.py
+++ quantize.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 
 def quantize(img, palette):
     if img.mode not in ('RGB', 'RGBA'):
@@ -10,26 +8,6 @@ def quantize(img, palette):
             color = img.getpixel((x, y))
             palette_color = find_closest(color, palette)
             img.putpixel((x, y), palette_color)
-            # very slow algorythm, don't use with images larger than 500x500 px
-            # apply_dithering(img, x, y, color, palette_color)
-
-
-def apply_dithering(img, x, y, color, palette_color):
-    width = img.size[0]
-    height = img.size[1]
-    err = color2nparr(color) - color2nparr(palette_color)
-    if x + 1 < width:
-        c1 = color2nparr(img.getpixel((x + 1, y)))
-        img.putpixel((x + 1, y), nparr2color(c1 + (err * (7 / 16))))
-    if y + 1 < height:
-        c2 = color2nparr(img.getpixel((x, y + 1)))
-        img.putpixel((x, y + 1), nparr2color(c2 + (err * (3 / 16))))
-    if y + 1 < height:
-        c3 = color2nparr(img.getpixel((x, y + 1)))
-        img.putpixel((x, y + 1), nparr2color(c3 + (err * (5 / 16))))
-    if x + 1 < width and y + 1 < height:
-        c4 = color2nparr(img.getpixel((x + 1, y + 1)))
-        img.putpixel((x + 1, y + 1), nparr2color(c4 + (err * (1 / 16))))
 
 
 def find_closest(color, palette):
@@ -49,27 +27,3 @@ def find_closest(color, palette):
             min_diff = diff
             closest_color = p
     return closest_color
-
-
-def nparr2color(arr):
-    if arr.shape[0] < 3:
-        raise Exception("Can't convert an array " + arr.shape + " to a color")
-    r = num2rgbchannel(arr[0])
-    g = num2rgbchannel(arr[1])
-    b = num2rgbchannel(arr[2])
-    return r, g, b
-
-
-def color2nparr(clr):
-    if len(clr) < 3:
-        raise Exception("Can't convert color " + clr + ", expected at least 3 channels")
-    return np.array([clr[0], clr[1], clr[2]])
-
-
-def num2rgbchannel(num):
-    int_num = int(num)
-    if int_num < 0:
-        int_num = 0
-    elif int_num > 255:
-        int_num = 255
-    return int_num
