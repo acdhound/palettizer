d090aa15d9746699e5da0b16687101eac37cff7c
acdhound <adivanov95@gmail.com>
2022-04-23 16:31:55 +0300

    example to render html template with a palette histogram

--- palettizer/htmlview.py
+++ palettizer/htmlview.py
@@ -8,7 +8,7 @@ from palettizer.imgutils import read_rgb_image
 
 def render_html(variables):
     env = Environment(
-        loader=PackageLoader("htmlview"), #this causes recursion while loading module 'htmlview', need to fix this
+        loader=PackageLoader("palettizer"),
         autoescape=select_autoescape()
     )
     template = env.get_template("template.html")
@@ -26,5 +26,18 @@ def np_image_to_base64(img: np.ndarray):
 
 img = read_rgb_image("C:\\Users\\adiva\\PycharmProjects\\palettizer\\palettizer\\resources\\bliss.jpg")
 img_base64 = np_image_to_base64(img)
-rendered_html = render_html({"img_base64": img_base64})
-print(rendered_html)
+
+colors = {
+    1: {'color': {'color': (255, 0, 0), 'name': 'red', 'vendor': 'ABC Paints'}, "pixels": 100},
+    2: {'color': {'color': (255, 255, 0), 'name': 'yellow', 'vendor': 'ABC Paints'}, "pixels": 60},
+    3: {'color': {'color': (0, 255, 0), 'name': 'green', 'vendor': 'ABC Paints'}, "pixels": 300},
+    4: {'color': {'color': (0, 0, 255), 'name': 'blue', 'vendor': 'ABC Paints'}, "pixels": 10}
+     }
+
+rendered_html = render_html({"img_base64": img_base64,
+                             "colors": colors})
+
+with open("./output.html", mode="w") as output:
+    output.write(rendered_html)
+
+exit(0)
