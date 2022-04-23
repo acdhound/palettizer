0e8a4b5a91de0be4a3bf9da5aabc4a2a3cbd022d
acdhound <adivanov95@gmail.com>
2022-04-23 19:44:07 +0300

    add a test case with quantization algorythm; specify parameter and return types for quantization functions

--- palettizer/test/test_htmlview.py
+++ palettizer/test/test_htmlview.py
@@ -1,21 +1,53 @@
 from palettizer.htmlview import image_and_palette_as_html
 from palettizer.imgutils import read_rgb_image
+from palettizer.quantize import quantize
 from testutils import get_test_resource
 
 
+PALETTE = [
+  {'color': (255, 0, 0), 'name': 'red', 'vendor': 'ABC Paints'},
+  {'color': (255, 255, 0), 'name': 'yellow', 'vendor': 'ABC Paints'},
+  {'color': (0, 255, 0), 'name': 'green', 'vendor': 'ABC Paints'},
+  {'color': (0, 0, 255), 'name': 'blue', 'vendor': 'ABC Paints'}
+]
+IMAGE_PATH = str(get_test_resource("bliss.jpg"))
+
+
 def test_image_and_palette_as_html():
-    img = read_rgb_image(str(get_test_resource("bliss.jpg")))
+    img = read_rgb_image(IMAGE_PATH)
     colors = {
-        1: {'color': {'color': (255, 0, 0), 'name': 'red', 'vendor': 'ABC Paints'}, "pixels": 10},
-        2: {'color': {'color': (255, 255, 0), 'name': 'yellow', 'vendor': 'ABC Paints'}, "pixels": 60},
-        3: {'color': {'color': (0, 255, 0), 'name': 'green', 'vendor': 'ABC Paints'}, "pixels": 300},
-        4: {'color': {'color': (0, 0, 255), 'name': 'blue', 'vendor': 'ABC Paints'}, "pixels": 100}
+        1: {'color': PALETTE[0], "pixels": 10},
+        2: {'color': PALETTE[1], "pixels": 20},
+        3: {'color': PALETTE[2], "pixels": 30},
+        4: {'color': PALETTE[3], "pixels": 40}
     }
 
     rendered_html = image_and_palette_as_html(img, colors)
 
     assert rendered_html is not None
-    assert "BDAQkJCQwLDBgNDRgyIRwhM" in rendered_html
-    assert "<td><div class=\"color-block\" style=\"background-color: rgb(0, 255, 0)\"></div></td>" in rendered_html
-    assert "<td>blue</td>" in rendered_html
+    assert len(rendered_html) > 100000
+    assert "<td><div class=\"color-block\" style=\"background-color: rgb(255, 0, 0)\"></div></td>" in rendered_html
+    assert "<td>red</td>" in rendered_html
     assert "<td>10</td>" in rendered_html
+    assert "<td><div class=\"color-block\" style=\"background-color: rgb(255, 255, 0)\"></div></td>" in rendered_html
+    assert "<td>yellow</td>" in rendered_html
+    assert "<td>20</td>" in rendered_html
+    assert "<td><div class=\"color-block\" style=\"background-color: rgb(0, 255, 0)\"></div></td>" in rendered_html
+    assert "<td>green</td>" in rendered_html
+    assert "<td>30</td>" in rendered_html
+    assert "<td><div class=\"color-block\" style=\"background-color: rgb(0, 0, 255)\"></div></td>" in rendered_html
+    assert "<td>blue</td>" in rendered_html
+    assert "<td>40</td>" in rendered_html
+
+
+def test_image_and_palette_as_html__with_quantize():
+    img, colors = quantize(IMAGE_PATH, PALETTE)
+
+    rendered_html = image_and_palette_as_html(img, colors)
+
+    assert rendered_html is not None
+    assert len(rendered_html) > 100000
+    assert "<td><div class=\"color-block\" style=\"background-color: rgb(0, 255, 0)\"></div></td>" in rendered_html
+    assert "<td><div class=\"color-block\" style=\"background-color: rgb(0, 0, 255)\"></div></td>" in rendered_html
+    assert "<td>green</td>" in rendered_html
+    assert "<td>blue</td>" in rendered_html
