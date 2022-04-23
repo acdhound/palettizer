75aac657e49c7200e7b061145b32ecd3140b8fc3
acdhound <adivanov95@gmail.com>
2022-04-23 21:03:21 +0300

    show percentage of a color instead of pixels and apply descending order

--- palettizer/htmlview.py
+++ palettizer/htmlview.py
@@ -5,10 +5,12 @@ from importlib import resources
 
 
 def image_and_palette_as_html(image: np.ndarray, palette_hist: dict):
+    colors_sorted = sorted(palette_hist.values(), key=lambda i: i['pixels'], reverse=True)
+    colors_percentage = map(lambda i: __pixels_to_percentage(i, image), colors_sorted)
     img_base64 = np_image_to_base64(image, "jpg")
     return __render_template("template.html", {
         "image": {"format": "jpg", "base64": img_base64},
-        "colors": palette_hist})
+        "colors": colors_percentage})
 
 
 def __load_template(name: str) -> str:
@@ -24,3 +26,8 @@ __ENV = Environment(loader=FunctionLoader(__load_template), autoescape=select_au
 def __render_template(template: str, variables: dict):
     template = __ENV.get_template(template)
     return template.render(variables)
+
+
+def __pixels_to_percentage(color: dict, img: np.ndarray) -> dict:
+    percentage = (color['pixels'] * 100.00) / (img.shape[0] * img.shape[1])
+    return {'color': color['color'], 'percentage': percentage}
--- palettizer/test/test_htmlview.py
+++ palettizer/test/test_htmlview.py
@@ -15,11 +15,12 @@ IMAGE_PATH = str(get_test_resource("bliss.jpg"))
 
 def test_image_and_palette_as_html():
     img = read_rgb_image(IMAGE_PATH)
+    area = (img.shape[0] * img.shape[1]) // 4
     colors = {
-        1: {'color': PALETTE[0], "pixels": 10},
-        2: {'color': PALETTE[1], "pixels": 20},
-        3: {'color': PALETTE[2], "pixels": 30},
-        4: {'color': PALETTE[3], "pixels": 40}
+        1: {'color': PALETTE[0], "pixels": area},
+        2: {'color': PALETTE[1], "pixels": area},
+        3: {'color': PALETTE[2], "pixels": area},
+        4: {'color': PALETTE[3], "pixels": area}
     }
 
     rendered_html = image_and_palette_as_html(img, colors)
@@ -28,16 +29,13 @@ def test_image_and_palette_as_html():
     assert len(rendered_html) > 100000
     assert "<td><div class=\"color-block\" style=\"background-color: rgb(255, 0, 0)\"></div></td>" in rendered_html
     assert "<td>red</td>" in rendered_html
-    assert "<td>10</td>" in rendered_html
     assert "<td><div class=\"color-block\" style=\"background-color: rgb(255, 255, 0)\"></div></td>" in rendered_html
     assert "<td>yellow</td>" in rendered_html
-    assert "<td>20</td>" in rendered_html
     assert "<td><div class=\"color-block\" style=\"background-color: rgb(0, 255, 0)\"></div></td>" in rendered_html
     assert "<td>green</td>" in rendered_html
-    assert "<td>30</td>" in rendered_html
     assert "<td><div class=\"color-block\" style=\"background-color: rgb(0, 0, 255)\"></div></td>" in rendered_html
     assert "<td>blue</td>" in rendered_html
-    assert "<td>40</td>" in rendered_html
+    assert "<td>25.0</td>" in rendered_html
 
 
 def test_image_and_palette_as_html__with_quantize():
