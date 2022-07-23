a8be3ea87780932e11c2adb23edf1cc1cc81b865
acdhound <adivanov95@gmail.com>
2022-07-23 23:10:16 +0200

    fix CLI

--- palettizer/__main__.py
+++ palettizer/__main__.py
@@ -1,5 +1,6 @@
 from . quantize import quantize
-from . palette import parse_palette
+from . palette import Palette
+from . htmlview import image_and_palette_as_html
 from skimage import io
 import sys
 
@@ -28,23 +29,28 @@ if len(sys.argv) > 4:
         raise Exception("Number of colors should be >= 0")
 
 print('Parsing the palette from ' + palette + '...')
-palette = parse_palette(palette)
+palette = Palette.from_files(palette.split(","))
 print("Successfully parsed")
 
 print('Quantizing the image from file ' + input_img + '...')
-result, palette_histogram = quantize(input_img, palette, n_colors)
+q_image = quantize(input_img, palette, n_colors)
 print('Quantization finished')
 
 print('Saving the quantized image to ' + output_img + '...')
-io.imsave(output_img, result)
+io.imsave(output_img, q_image.image)
 print('Successfully saved')
 
 print("Palette colors usage:")
-palette_histogram_sorted = sorted(palette_histogram.values(), key=lambda i: i['pixels'])
-palette_histogram_sorted.reverse()
-image_area = result.shape[0] * result.shape[1]
-for item in palette_histogram_sorted:
-    area_percentage = (item['pixels'] / image_area) * 100
-    print(f"Color: {item['color']['name']} {item['color']['vendor']}, area: {area_percentage} %")
+colors_sorted = sorted(q_image.color_pixels.items(), key=lambda i: i[1], reverse=True)
+image_area = q_image.image.shape[0] * q_image.image.shape[1]
+for item in colors_sorted:
+    area_percentage = (item[1] / image_area) * 100
+    print(f"Color: {item[0].name} {item[0].vendor}, area: {area_percentage} %")
+
+html_file = output_img + ".html"
+print("Saving results to HTML file " + html_file)
+html_result = image_and_palette_as_html(q_image)
+with open(html_file, 'w') as f:
+    f.write(html_result)
 
 exit(0)
