764b11160e34bb18b2abd12dcf2f59424cce9944
acdhound <adivanov95@gmail.com>
2022-07-23 20:09:12 +0200

    don't store the current step in the context, add palette buttons and a callback handler

--- palettizerbot/__main__.py
+++ palettizerbot/__main__.py
@@ -1,9 +1,9 @@
 # todo this is a hack to configure logging before a logger is initialized in each imported module
 from . import initlogging
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler
 from telegram.ext.filters import Filters
 import sys
-from . handlers2 import on_start, on_error, on_picture, on_palette, on_text
+from . handlers2 import on_start, on_error, on_picture, on_query, on_text
 import logging
 
 
@@ -21,12 +21,13 @@ def main():
     start_handler = CommandHandler(command='start', callback=on_start, run_async=True)
     dispatcher.add_handler(start_handler)
 
-    img_filter = Filters.photo | Filters.document.image
-    picture_handler = MessageHandler(filters=img_filter, callback=on_picture, run_async=True)
+    picture_handler = MessageHandler(filters=(Filters.photo | Filters.document.image),
+                                     callback=on_picture,
+                                     run_async=True)
     dispatcher.add_handler(picture_handler)
 
-    palette_handler = CommandHandler(command='palette', callback=on_palette, run_async=True)
-    dispatcher.add_handler(palette_handler)
+    callback_handler = CallbackQueryHandler(callback=on_query, run_async=True)
+    dispatcher.add_handler(callback_handler)
 
     text_handler = MessageHandler(filters=Filters.text, callback=on_text, run_async=True)
     dispatcher.add_handler(text_handler)
--- palettizerbot/handlers2.py
+++ palettizerbot/handlers2.py
@@ -1,7 +1,8 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from telegram.ext import CallbackContext
 import logging
-from . userstep import UserStep
+from palettizer.palette import Palette
+from typing import Optional
 
 
 logger = logging.getLogger(__name__)
@@ -18,11 +19,90 @@ def on_start(update: Update, context: CallbackContext):
 Hi, I'm Palettizer bot. I will pick the right colors for you to paint anything.
 Send me the picture you'd like paint.
     """
-    __set_step_to_context(context, UserStep())
     context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 
 def on_picture(update: Update, context: CallbackContext):
+    picture: bytes = __read_picture_as_bytes(update, context)
+    if picture is not None:
+        __set_picture_to_context(context, picture)
+        __send_palettes(update, context)
+
+
+def on_query(update: Update, context: CallbackContext):
+    query = update.callback_query
+    tokens = query.data.split(" ")
+    if tokens[0] == "palette":
+        __set_palette_to_context(context, tokens[1])
+        __request_n_colors(query)
+    elif tokens[0] == "processing":
+        pass
+
+
+def on_text(update: Update, context: CallbackContext):
+    pass
+
+
+def __send_palettes(update: Update, context: CallbackContext):
+    inline_markup: InlineKeyboardMarkup = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton(text="Montana Black", callback_data="palette mtnblack"),
+            InlineKeyboardButton(text="See colors", url="https://google.com")
+        ],
+        [
+            InlineKeyboardButton(text="Montana 94", callback_data="palette mtn94"),
+            InlineKeyboardButton(text="See colors", url="https://google.com")
+        ],
+        [
+            InlineKeyboardButton(text="Arton", callback_data="palette arton"),
+            InlineKeyboardButton(text="See colors", url="https://google.com")
+        ],
+        [
+            InlineKeyboardButton(text="Tikkurila", callback_data="palette tikkurila"),
+            InlineKeyboardButton(text="See colors", url="https://google.com")
+        ],
+    ])
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text="Which palette would you like to use?",
+                             reply_markup=inline_markup)
+
+
+def __request_n_colors(query: CallbackQuery):
+    query.edit_message_text("How many colors would you like?")
+    query.edit_message_reply_markup(InlineKeyboardMarkup([
+        [InlineKeyboardButton(text="No limits", callback_data="no_colors")]
+    ]))
+
+
+def __get_picture_from_context(context: CallbackContext):
+    picture = context.user_data["picture"]
+    if not isinstance(picture, bytes):
+        raise Exception("Can't get the picture from the context")
+    return picture
+
+
+def __set_picture_to_context(context: CallbackContext, picture: bytes):
+    context.user_data["picture"] = picture
+
+
+def __get_palette_from_context(context: CallbackContext):
+    palette = context.user_data["palette"]
+    if not isinstance(palette, Palette):
+        raise Exception("Can't get the palette from the context")
+    return palette
+
+
+def __set_palette_to_context(context: CallbackContext, palette_id: str):
+    try:
+        palette = Palette.from_predefined([palette_id])
+    except Exception as e:
+        raise Exception("Failed to find a palette by ID " + palette_id, e)
+    if palette is None:
+        raise Exception("Palette for ID {} is None".format(palette_id))
+    context.user_data["palette"] = palette
+
+
+def __read_picture_as_bytes(update: Update, context: CallbackContext) -> Optional[bytes]:
     if not update.message:
         raise Exception("Can't get a message from an Update")
     if not update.message.document and not update.message.photo:
@@ -33,7 +113,7 @@ def on_picture(update: Update, context: CallbackContext):
         if not mime_type or (not mime_type.startswith("image")):
             context.bot.send_message(chat_id=update.effective_chat.id,
                                      text="File is not an image")
-            return
+            return None
         file = update.message.document.get_file()
     else:
         photos = update.message.photo
@@ -46,45 +126,11 @@ def on_picture(update: Update, context: CallbackContext):
     if file.file_size > 10 * 1024 * 1024:
         context.bot.send_message(chat_id=update.effective_chat.id,
                                  text="Please send a picture up to 10 MB size")
-        return
+        return None
 
     try:
         image_as_bytearray: bytes = file.download_as_bytearray()
         logger.debug(f"A file of {len(image_as_bytearray)} bytes downloaded from the message")
+        return image_as_bytearray
     except Exception as e:
         raise IOError("Failed to download file attached to the message", e)
-
-    __set_step_to_context(context, UserStep())
-    __set_picture_to_context(context, image_as_bytearray)
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Picture was successfully uploaded")
-
-
-def on_palette(update: Update, context: CallbackContext):
-    pass
-
-
-def on_text(update: Update, context: CallbackContext):
-    pass
-
-
-def __get_step_from_context(context: CallbackContext) -> UserStep:
-    step = context.user_data["step"]
-    if not isinstance(step, UserStep):
-        raise Exception("Invalid user step in the context")
-    return step
-
-
-def __set_step_to_context(context: CallbackContext, step: UserStep):
-    context.user_data["step"] = step
-
-
-def __get_picture_from_context(context: CallbackContext):
-    picture = context.user_data["picture"]
-    if not isinstance(picture, bytes):
-        raise Exception("Can't get the picture from the context")
-    return picture
-
-
-def __set_picture_to_context(context: CallbackContext, picture: bytes):
-    context.user_data["picture"] = picture
