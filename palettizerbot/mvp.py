e723b3feecc517f28fe43123f6052002225a146e
acdhound <adivanov95@gmail.com>
2022-09-21 13:45:17 +0200

    implement model

--- palettizerbot/mvp.py
+++ palettizerbot/mvp.py
@@ -1,20 +1,59 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 from palettizer.palette import Palette
+from typing import Union
 
 
 class Model:
     def __init__(self, context: CallbackContext):
         self.context = context
 
-    def set_image(self, image: bytearray):
-        pass
+    def set_picture(self, picture: Union[bytes, bytearray]):
+        self.context.user_data["picture"] = picture
 
-    def add_palette(self, palette: Palette):
-        pass
+    def get_picture(self):
+        if "picture" not in self.context.user_data:
+            return None
+        picture = self.context.user_data["picture"]
+        if not isinstance(picture, bytes) and not isinstance(picture, bytearray):
+            raise Exception("Can't get the picture from the context")
+        return picture
 
-    def set_num_colors(self, num: int):
-        pass
+    def add_palette(self, palette_id: str, palette: Palette):
+        if palette is None:
+            raise Exception("Palette can't be None")
+        if palette_id is None:
+            raise Exception("ID can't be None")
+        palettes = self.context.user_data["palettes"]
+        if not palettes:
+            palettes = {palette_id: palette}
+            self.context.user_data["palettes"] = palettes
+        elif isinstance(palettes, dict):
+            palettes[palette_id] = palette
+            self.context.user_data["palettes"] = palettes
+        else:
+            raise Exception("Context object with key 'palettes' has a wrong type:"
+                            " expected dict, actual " + type(palettes))
+
+    def get_palettes(self) -> dict:
+        if "palettes" not in self.context.user_data:
+            return {}
+        palettes = self.context.user_data["palettes"]
+        if not isinstance(palettes, dict):
+            raise Exception("Can't get the palettes from the context")
+        return palettes
+
+    def set_num_colors(self, n_colors: int):
+        if n_colors <= 0:
+            n_colors = 0
+        self.context.user_data["n_colors"] = n_colors
+
+    def get_num_colors(self):
+        n_colors = self.context.user_data["n_colors"]
+        if n_colors is not None and isinstance(n_colors, int):
+            return n_colors
+        else:
+            return 0
 
 
 class Presenter:
